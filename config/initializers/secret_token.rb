b5c2d7f480351db385501a412df3c7b4bf5c32b7
simsicon <simsicon@gmail.com>
2013-05-27 14:04:10 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EchidnaQq::Application.config.secret_token = 'ed0a7dc26512337d45c47aaee2005af3c72371b9d8291f3bcdbbe0f8ffcc993a3df9fe0639baee0819d4a0341ea29487f82980de8dcf23e157ac7444e3676ac4'
